f555a588388a8011498a6f0863adbf402d1e3e68
Anton Rokhin <rohesrodriges@gmail.com>
2020-03-04 22:39:56 +0300

    Blog. Iteration 2-4

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
+  validates_presence_of :post_id
+  validates_presence_of :body
 end
+
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer         not null, primary key
+#  post_id    :integer         not null
+#  body       :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
-  has_many :comments, dependent: :delete_all
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  validates_presence_of :title
+  validates_presence_of :body
+
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer         not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
